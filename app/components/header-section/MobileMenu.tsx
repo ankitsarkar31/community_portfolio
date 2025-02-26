725e0a5e5fb91d20d8cac43604dcfafa482988f8
Ankit Sarkar <92983704+ankitsarkar31@users.noreply.github.com>
2025-02-26 13:06:36 +0530

    Update MobileMenu.tsx
    updated linkedin and github username

--- app/components/header-section/MobileMenu.tsx
+++ app/components/header-section/MobileMenu.tsx
@@ -52,7 +52,7 @@ export default function MobileMenu({
         <Link
           className="p-4 flex-1 flex justify-center w-full rounded-xl h-fit text-4xl visited:bg-[#E3D3BE] bg-gradient-to-r from-[#d9d9d915] to-[#7373731f] std-backdrop-blur"
           target="_blank"
-          href="https://linkedin.com/in/your_username"
+          href="https://linkedin.com/in/ankit-sarkar31"
           data-blobity-radius="10"
         >
           <Icon icon="hugeicons:linkedin-01" />
@@ -62,7 +62,7 @@ export default function MobileMenu({
           <Link
             className="p-4 flex justify-center w-full rounded-xl h-fit text-2xl bg-gradient-to-r from-[#d9d9d915] to-[#7373731f] std-backdrop-blur"
             target="_blank"
-            href="https://github.com/your_username"
+            href="https://github.com/ankitsarkar31"
             data-blobity-radius="10"
           >
             <Icon icon="hugeicons:github" />
